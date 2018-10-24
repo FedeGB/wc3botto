74ee2afafd400dee84ba87e206b8c45a90b41dbb
FedeGB <fedegb@fedenet.fed>
2018-10-23 22:19:05 -0300

    Fixed minor issue on case-sensitive.

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if(message.includes('punto')) {
+    if(message.toLowerCase().includes('punto')) {
         bot.sendMessage({
             to: channelID,
             message: "*puto"
