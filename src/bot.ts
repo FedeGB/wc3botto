e8239c105515653888dc268d82e99ab2320d1cae
Jere Aagaard <jereaa2@gmail.com>
2018-10-31 11:27:44 -0300

    :musical_note: Added Anthem to play at midnight

--- src/bot.ts
+++ src/bot.ts
@@ -31,6 +31,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     logger.log('info', `Logged in as ${client.user.tag}`);
+    scheduleAnthem();
 });
 
 client.on('error', (error: Error) => {
@@ -86,16 +87,13 @@ client.on('message', (message: Discord.Message) => {
                 if (message.member.voiceChannel && args.length > 1) {
                     let voiceConnection = client.voiceConnections.get(message.guild.id)
                     if (voiceConnection) {
-                        const fileDir = __dirname + '/../assets/audio/';
                         switch (args[1]) {
                             case 'marcha':
-                                voiceConnection.playFile(fileDir + 'marcha.mp3')
-                                    .on('error', (error: Error) => logger.log('error', error.message));
+                                playAudioFile(voiceConnection, 'marcha.mp3', 0.5);
                                 break;
 
                             case 'himno':
-                                voiceConnection.playFile(fileDir + 'himno-arg.mp3')
-                                    .on('error', (error: Error) => logger.log('error', error.message));
+                                playAudioFile(voiceConnection, 'himno-arg.mp3', 0.5);
                                 break;
                         
                             default:
@@ -129,9 +127,46 @@ const explain = (channel: Discord.TextChannel | Discord.DMChannel | Discord.Grou
         '!play   - Reproducir música en el canal de voz en el que está el bot (marcha / himno).\n' +
         '!stop   - Parar la música que está reproduciendo.\n' +
         '!kick   - Sacar al bot del canal de voz.\n' +
-        '!help   - Este texto',
+        '!help   - Este texto.',
         { code: true }
     );
+}
+
+const playAudioFile = (voiceConnection: Discord.VoiceConnection, fileName: string, volume: number = 1): Discord.StreamDispatcher => {
+    const filePath = __dirname + '/../assets/audio/' + fileName;
+    let dispatcher = voiceConnection.playFile(filePath, { volume: volume });
+
+    dispatcher.on('error', (error: Error) => {
+        logger.log('error', error.message);
+    });
+
+    return dispatcher;
+}
+
+const scheduleAnthem = () => {
+    const now = new Date(Date.now());
+    let tomorrow = new Date(now);
+    tomorrow.setDate(now.getDate() + 1);
+    tomorrow.setUTCHours(3, 0, 0, 0);       // Midnight in Argentina
+    
+    client.setTimeout(() => {
+        client.channels.forEach((channel: Discord.Channel) => {
+            if (channel.type === 'voice') {
+                const voiceChannel: Discord.VoiceChannel = channel as Discord.VoiceChannel;
+                if (voiceChannel.members.size > 0) {
+                    voiceChannel.join()
+                        .then((voiceConnection: Discord.VoiceConnection) => {
+                            const dispatcher = playAudioFile(voiceConnection, 'himno-arg.mp3', 0.3);
+                            client.setTimeout(() => {
+                                dispatcher.end();
+                                voiceConnection.disconnect();
+                            }, (3 * 60 + 58) * 1000);
+                        });
+                }
+            }
+        });
+        scheduleAnthem();
+    }, tomorrow.valueOf() - now.valueOf());
 }
 
 client.login(CONFIG.token);
